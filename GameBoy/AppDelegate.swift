ce3b86d1c040f45c496485ece150f9f4428b6bf7
Cole van Krieken <colevankrieken@gmail.com>
2017-07-14 14:43:38 +0200

    Initial Commit

---
+++ GameBoy/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  GameBoy
+//
+//  Created by Cole van Krieken on 7/14/17.
+//  Copyright (c) 2017 Cole van Krieken. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
